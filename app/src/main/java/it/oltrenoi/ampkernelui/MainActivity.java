14fb7b079036d6443355e443feb4d2448084b952
kruntz <carlo@ottolina.net>
2017-02-03 23:42:31 +0100

    Cleanup

--- app/src/main/java/it/oltrenoi/ampkernelui/MainActivity.java
+++ app/src/main/java/it/oltrenoi/ampkernelui/MainActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2016, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package it.oltrenoi.ampkernelui;
 
 import android.app.Activity;
@@ -100,7 +84,8 @@ public class MainActivity extends Activity {
         paint.setUnderlineText(true);
         paint.setFakeBoldText(true);
         paint.setTextSize(8f);
-        oledDisplay.getCanvas().drawText("Kick", 0f, 0f, paint);
+        oledDisplay.getCanvas().drawText("View Group 01", 0f, 0f, paint);
+        oledDisplay.displayImage();
     }
 
     @Override
--- app/src/main/java/it/oltrenoi/ampkernelui/oled/Ssd1306.java
+++ app/src/main/java/it/oltrenoi/ampkernelui/oled/Ssd1306.java
@@ -12,13 +12,18 @@ import java.io.IOException;
 public class Ssd1306 {
 
     private static final short SSD1306_I2C_ADDRESS = 0x3C;
+
     private static final short SSD1306_SETCONTRAST = 0x81;
+
     private static final short SSD1306_DISPLAYALLON_RESUME = 0xA4;
     private static final short SSD1306_DISPLAYALLON = 0xA5;
+
     private static final short SSD1306_NORMALDISPLAY = 0xA6;
     private static final short SSD1306_INVERTDISPLAY = 0xA7;
+
     private static final short SSD1306_DISPLAYOFF = 0xAE;
     private static final short SSD1306_DISPLAYON = 0xAF;
+
     private static final short SSD1306_SETDISPLAYOFFSET = 0xD3;
     private static final short SSD1306_SETCOMPINS = 0xDA;
     private static final short SSD1306_SETVCOMDETECT = 0xDB;
@@ -28,22 +33,25 @@ public class Ssd1306 {
     private static final short SSD1306_SETLOWCOLUMN = 0x00;
     private static final short SSD1306_SETHIGHCOLUMN = 0x10;
     private static final short SSD1306_SETSTARTLINE = 0x40;
+
     private static final short SSD1306_MEMORYMODE = 0x20;
     private static final short SSD1306_COLUMNADDR = 0x21;
     private static final short SSD1306_PAGEADDR = 0x22;
+
     private static final short SSD1306_COMSCANINC = 0xC0;
     private static final short SSD1306_COMSCANDEC = 0xC8;
     private static final short SSD1306_SEGREMAP = 0xA0;
     private static final short SSD1306_CHARGEPUMP = 0x8D;
     private static final short SSD1306_EXTERNALVCC = 0x1;
     private static final short SSD1306_SWITCHCAPVCC = 0x2;
-    private static final short SSD1306_ACTIVATE_SCROLL = 0x2F;
-    private static final short SSD1306_DEACTIVATE_SCROLL = 0x2E;
+
     private static final short SSD1306_SET_VERTICAL_SCROLL_AREA = 0xA3;
     private static final short SSD1306_RIGHT_HORIZONTAL_SCROLL = 0x26;
     private static final short SSD1306_LEFT_HORIZONTAL_SCROLL = 0x27;
     private static final short SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
     private static final short SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
+    private static final short SSD1306_ACTIVATE_SCROLL = 0x2F;
+    private static final short SSD1306_DEACTIVATE_SCROLL = 0x2E;
 
     private static final String TAG = Ssd1306.class.getSimpleName();
 
