55ac7353ca22513cb85eea5de1e4dc29634e254d
kruntz <carlo@ottolina.net>
2017-02-05 00:38:22 +0100

    OLED restruct

--- app/src/main/java/it/oltrenoi/ampkernelui/MainActivity.java
+++ app/src/main/java/it/oltrenoi/ampkernelui/MainActivity.java
@@ -20,14 +20,8 @@ import it.oltrenoi.ampkernelui.oled.Ssd1306;
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    // SPI Device Name
-    private static final String SPI_DEVICE_NAME = "SPITEST";
-
     // I2C Device Name
     private static final String I2C_DEVICE_NAME = "I2CTEST";
-    // I2C Slave Address
-    private static final int I2C_ADDRESS = 0x7a;
-
 
     private SpiDevice spiDevice;
     private I2cDevice i2cDevice;
@@ -47,26 +41,12 @@ public class MainActivity extends Activity {
             Log.i(TAG, "List of available devices: " + spiBusList);
         }
 
-        // Attempt to access the SPI device
-        try {
-            spiDevice = manager.openSpiDevice(SPI_DEVICE_NAME);
-        } catch (IOException e) {
-            Log.w(TAG, "Unable to access SPI device", e);
-        }
-
         // Enumerate I2C busses
         List<String> i2cBusList = manager.getI2cBusList();
         if (i2cBusList.isEmpty()) {
             Log.i(TAG, "No I2C bus available on this device.");
         } else {
             Log.i(TAG, "List of available devices: " + i2cBusList);
-        }
-
-        // Attempt to access the I2C device
-        try {
-            i2cDevice = manager.openI2cDevice(I2C_DEVICE_NAME, I2C_ADDRESS);
-        } catch (IOException e) {
-            Log.w(TAG, "Unable to access I2C device", e);
         }
 
         // Enumerate GPIO ports
@@ -77,15 +57,18 @@ public class MainActivity extends Activity {
             Log.i(TAG, "List of available ports: " + portList);
         }
 
-        Ssd1306 oledDisplay = new Ssd1306(i2cDevice);
-        oledDisplay.begin();
-        Paint paint = new Paint();
+        try (Ssd1306 oledDisplay = new Ssd1306(I2C_DEVICE_NAME)) {
+            oledDisplay.begin();
+            Paint paint = new Paint();
 
-        paint.setUnderlineText(true);
-        paint.setFakeBoldText(true);
-        paint.setTextSize(8f);
-        oledDisplay.getCanvas().drawText("View Group 01", 0f, 0f, paint);
-        oledDisplay.displayImage();
+            paint.setUnderlineText(true);
+            paint.setFakeBoldText(true);
+            paint.setTextSize(8f);
+            oledDisplay.getCanvas().drawText("View Group 01", 0f, 0f, paint);
+            oledDisplay.displayImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- app/src/main/java/it/oltrenoi/ampkernelui/oled/Ssd1306.java
+++ app/src/main/java/it/oltrenoi/ampkernelui/oled/Ssd1306.java
@@ -6,57 +6,72 @@ import android.graphics.Color;
 import android.util.Log;
 
 import com.google.android.things.pio.I2cDevice;
+import com.google.android.things.pio.PeripheralManagerService;
 
+import java.io.Closeable;
 import java.io.IOException;
 
-public class Ssd1306 {
-
-    private static final short SSD1306_I2C_ADDRESS = 0x3C;
-
-    private static final short SSD1306_SETCONTRAST = 0x81;
-
-    private static final short SSD1306_DISPLAYALLON_RESUME = 0xA4;
-    private static final short SSD1306_DISPLAYALLON = 0xA5;
-
-    private static final short SSD1306_NORMALDISPLAY = 0xA6;
-    private static final short SSD1306_INVERTDISPLAY = 0xA7;
-
-    private static final short SSD1306_DISPLAYOFF = 0xAE;
-    private static final short SSD1306_DISPLAYON = 0xAF;
-
-    private static final short SSD1306_SETDISPLAYOFFSET = 0xD3;
-    private static final short SSD1306_SETCOMPINS = 0xDA;
-    private static final short SSD1306_SETVCOMDETECT = 0xDB;
-    private static final short SSD1306_SETDISPLAYCLOCKDIV = 0xD5;
-    private static final short SSD1306_SETPRECHARGE = 0xD9;
-    private static final short SSD1306_SETMULTIPLEX = 0xA8;
-    private static final short SSD1306_SETLOWCOLUMN = 0x00;
-    private static final short SSD1306_SETHIGHCOLUMN = 0x10;
-    private static final short SSD1306_SETSTARTLINE = 0x40;
-
-    private static final short SSD1306_MEMORYMODE = 0x20;
-    private static final short SSD1306_COLUMNADDR = 0x21;
-    private static final short SSD1306_PAGEADDR = 0x22;
-
-    private static final short SSD1306_COMSCANINC = 0xC0;
-    private static final short SSD1306_COMSCANDEC = 0xC8;
-    private static final short SSD1306_SEGREMAP = 0xA0;
-    private static final short SSD1306_CHARGEPUMP = 0x8D;
-    private static final short SSD1306_EXTERNALVCC = 0x1;
-    private static final short SSD1306_SWITCHCAPVCC = 0x2;
-
-    private static final short SSD1306_SET_VERTICAL_SCROLL_AREA = 0xA3;
-    private static final short SSD1306_RIGHT_HORIZONTAL_SCROLL = 0x26;
-    private static final short SSD1306_LEFT_HORIZONTAL_SCROLL = 0x27;
-    private static final short SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
-    private static final short SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
-    private static final short SSD1306_ACTIVATE_SCROLL = 0x2F;
-    private static final short SSD1306_DEACTIVATE_SCROLL = 0x2E;
+public class Ssd1306 implements Closeable {
 
     private static final String TAG = Ssd1306.class.getSimpleName();
 
+    // I2C Slave Address
+    private static final int I2C_ADDRESS = 0x78;
+
+    // Display size
     private static final int DISPLAY_WIDTH = 128;
     private static final int DISPLAY_HEIGHT = 64;
+
+    private static final int COMMAND_DISPLAY_ON = 0xAF;
+    private static final int COMMAND_DISPLAY_OFF = 0xAE;
+
+    private static final int SET_STARTLINE = 0x40;
+
+    private static final int INIT_SEGREMAP = 0xA0;
+
+    private static final int INIT_COMSCANDEC = 0xC8;
+    private static final int INIT_DUTY_CYCLE_1_64 = 0x3F;
+
+    private static final int INIT_CLK_DIV = 0xD5;
+    private static final int INIT_RESISTER_RATIO = 0x80;
+
+    private static final int INIT_MEMORY_MODE = 0x20;
+    private static final int INIT_MEMORY_HORIZ = 0x00;
+
+    private static final int INIT_DISPLAY_OFFSET = 0xD3;
+    private static final int INIT_DISPLAY_NO_OFFSET = 0x00;
+
+    private static final int SSD1306_SETCONTRAST = 0x81;
+
+    private static final int SSD1306_DISPLAYALLON_RESUME = 0xA4;
+    private static final int SSD1306_DISPLAYALLON = 0xA5;
+
+    private static final int SSD1306_NORMALDISPLAY = 0xA6;
+    private static final int SSD1306_INVERTDISPLAY = 0xA7;
+
+    private static final int SSD1306_SETCOMPINS = 0xDA;
+    private static final int SSD1306_SETVCOMDETECT = 0xDB;
+    private static final int SSD1306_SETPRECHARGE = 0xD9;
+    private static final int COMMAND_SETMULTIPLEX = 0xA8;
+    private static final int SSD1306_SETLOWCOLUMN = 0x00;
+    private static final int SSD1306_SETHIGHCOLUMN = 0x10;
+
+    private static final int SSD1306_COLUMNADDR = 0x21;
+    private static final int SSD1306_PAGEADDR = 0x22;
+
+    private static final int SSD1306_COMSCANINC = 0xC0;
+    private static final int SSD1306_CHARGEPUMP = 0x8D;
+    private static final int SSD1306_EXTERNALVCC = 0x1;
+    private static final int SSD1306_SWITCHCAPVCC = 0x2;
+
+    // Scroll
+    private static final short COMMAND_ACTIVATE_SCROLL = 0x2F;
+    private static final short COMMAND_DEACTIVATE_SCROLL = 0x2E;
+    private static final short COMMAND_RIGHT_HORIZONTAL_SCROLL = 0x26;
+    private static final short COMMAND_LEFT_HORIZONTAL_SCROLL = 0x27;
+    private static final short COMMAND_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
+    private static final short COMMAND_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
+    private static final short SET_VERTICAL_SCROLL_AREA = 0xA3;
 
     private Bitmap bitmap;
     private Canvas canvas;
@@ -64,8 +79,9 @@ public class Ssd1306 {
     private I2cDevice i2cDevice;
     private byte[] buffer;
 
-    public Ssd1306(I2cDevice i2cDevice) {
-        this.i2cDevice = i2cDevice;
+    public Ssd1306(String i2DeviceName) throws IOException {
+        this.i2cDevice = new PeripheralManagerService().openI2cDevice(i2DeviceName, I2C_ADDRESS);
+        init();
         this.pages = (DISPLAY_HEIGHT / 8);
         this.buffer = new byte[DISPLAY_WIDTH * this.pages];
 
@@ -77,61 +93,69 @@ public class Ssd1306 {
         return canvas;
     }
 
-    private void initDisplay() {
-        if (DISPLAY_WIDTH == 128 && DISPLAY_HEIGHT == 64) {
-            this.init(0x3F, 0x12, 0x80);
-        } else if (DISPLAY_WIDTH == 128 && DISPLAY_HEIGHT == 32) {
-            this.init(0x1F, 0x02, 0x80);
-        } else if (DISPLAY_WIDTH == 96 && DISPLAY_HEIGHT == 16) {
-            this.init(0x0F, 0x02, 0x60);
+    @Override
+    public void close() throws IOException {
+        Log.d(TAG, "close");
+        if (this.i2cDevice != null) {
+            try {
+                this.i2cDevice.close();
+            } finally {
+                this.i2cDevice = null;
+            }
         }
     }
 
-    private void init(int multiplex, int compins, int ratio) {
-        this.command(SSD1306_DISPLAYOFF);
-        this.command(SSD1306_SETDISPLAYCLOCKDIV);
-        this.command((short) ratio);
-        this.command(SSD1306_SETMULTIPLEX);
-        this.command((short) multiplex);
-        this.command(SSD1306_SETDISPLAYOFFSET);
-        this.command((short) 0x0);
-        this.command(SSD1306_SETSTARTLINE);
+    private void init() {
+        Log.d(TAG, "init");
+
+        // Recommended initialization sequence based on http://goo.gl/VSu0C8
+        // Step 1: Start with the display off
+        this.command(COMMAND_DISPLAY_OFF);
+
+        // Step 2: Set up the required communication / power settings
+        this.command(INIT_SEGREMAP | 0x1);
+
+        this.command(INIT_COMSCANDEC);
+        this.command(INIT_DUTY_CYCLE_1_64);
+
+        this.command(INIT_CLK_DIV);
+        this.command(INIT_RESISTER_RATIO);
+
+        // Step 3: Set display input configuration and start.
+        // This will start the display all on
+        // START_LINE present the memory-based mBuffer to the screen
+        this.command(INIT_MEMORY_MODE);
+        this.command(INIT_MEMORY_HORIZ);
+
+        this.command(INIT_DISPLAY_OFFSET);
+        this.command(INIT_DISPLAY_NO_OFFSET);
+
+        this.command(COMMAND_DISPLAY_ON);
+
         this.command(SSD1306_CHARGEPUMP);
-        this.command((short) 0x10);
-        this.command(SSD1306_MEMORYMODE);
-        this.command((short) 0x00);
-        this.command((short) (SSD1306_SEGREMAP | 0x1));
-        this.command(SSD1306_COMSCANDEC);
-        this.command(SSD1306_SETCOMPINS);
-        this.command((short) compins);
-        this.command(SSD1306_SETCONTRAST);
-        this.command((short) 0x9F);
-        this.command(SSD1306_SETPRECHARGE);
-        this.command((short) 0x22);
-        this.command(SSD1306_SETVCOMDETECT);
-        this.command((short) 0x40);
-        this.command(SSD1306_DISPLAYALLON_RESUME);
-        this.command(SSD1306_NORMALDISPLAY);
-    }
 
-    private void command(int command) {
-        this.i2cWrite(0, command);
-    }
+        // OPTIONALS
+        //        this.command(COMMAND_SETMULTIPLEX);
+        //        this.command(SSD1306_CHARGEPUMP);
+        //        this.command(0x10);
+        //        this.command(SSD1306_SETCOMPINS);
+        //        this.command(0x12);
+        //        this.command(SSD1306_SETCONTRAST);
+        //        this.command(0x9F);
+        //        this.command(SSD1306_SETPRECHARGE);
+        //        this.command(0x22);
+        //        this.command(SSD1306_SETVCOMDETECT);
+        //        this.command(0x40);
+        //        this.command(SSD1306_DISPLAYALLON_RESUME);
+        //        this.command(SSD1306_NORMALDISPLAY);
+        // OPTIONALS
 
-    private void data(byte[] data) {
-//        for (int i = 0; i < data.length; i += 16) {
-//            this.i2cWrite(0x40, data[i]);
-//        }
-        try {
-            this.i2cDevice.write(data, data.length);
-        } catch (IOException e) {
-            Log.w(TAG, "Unable write buffer to I2C device", e);
-        }
+        this.command(SET_STARTLINE);
     }
 
     public void begin() {
-        this.initDisplay();
-        this.command(SSD1306_DISPLAYON);
+        this.init();
+        this.command(COMMAND_DISPLAY_ON);
         this.clear();
         this.display();
     }
@@ -171,18 +195,18 @@ public class Ssd1306 {
     }
 
     public void scrollHorizontally(boolean left, int start, int end) {
-        this.command(left ? SSD1306_LEFT_HORIZONTAL_SCROLL : SSD1306_RIGHT_HORIZONTAL_SCROLL);
+        this.command(left ? COMMAND_LEFT_HORIZONTAL_SCROLL : COMMAND_RIGHT_HORIZONTAL_SCROLL);
         this.command(0);
         this.command(start);
         this.command(0);
         this.command(end);
         this.command(1);
         this.command(0xFF);
-        this.command(SSD1306_ACTIVATE_SCROLL);
+        this.command(COMMAND_ACTIVATE_SCROLL);
     }
 
     public void stopScroll() {
-        this.command(SSD1306_DEACTIVATE_SCROLL);
+        this.command(COMMAND_DEACTIVATE_SCROLL);
     }
 
     public int getWidth() {
@@ -223,6 +247,18 @@ public class Ssd1306 {
         }
 
         this.display();
+    }
+
+    private void command(int command) {
+        this.i2cWrite(0, command);
+    }
+
+    private void data(byte[] data) {
+        try {
+            this.i2cDevice.write(data, data.length);
+        } catch (IOException e) {
+            Log.w(TAG, "Unable write buffer to I2C device", e);
+        }
     }
 
     private void i2cWrite(int register, int value) {
