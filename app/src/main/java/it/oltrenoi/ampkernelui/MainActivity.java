fb9830b9e3ba48541c288a59caa51f090e0fc4ed
kruntz <carlo@ottolina.net>
2017-02-07 15:05:34 +0100

    OLED I2C Working Sample

--- app/src/main/java/it/oltrenoi/ampkernelui/MainActivity.java
+++ app/src/main/java/it/oltrenoi/ampkernelui/MainActivity.java
@@ -1,8 +1,11 @@
 package it.oltrenoi.ampkernelui;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import com.google.android.things.pio.I2cDevice;
@@ -12,7 +15,9 @@ import com.google.android.things.pio.SpiDevice;
 import java.io.IOException;
 import java.util.List;
 
+import it.oltrenoi.ampkernelui.oled.BitmapHelper;
 import it.oltrenoi.ampkernelui.oled.Ssd1306;
+import it.oltrenoi.ampkernelui.oled.Ssd1306tmp;
 
 /**
  * Skeleton of the main AMP Kernel and UI activity.
@@ -22,9 +27,52 @@ public class MainActivity extends Activity {
 
     // I2C Device Name
     private static final String I2C_DEVICE_NAME = "I2C1";
-
+    private static final int FPS = 30; // Frames per second on draw thread
+    private static final int BITMAP_FRAMES_PER_MOVE = 4; // Frames to show bitmap before moving it
     private SpiDevice spiDevice;
     private I2cDevice i2cDevice;
+    private boolean mExpandingPixels = true;
+    private int mDotMod = 1;
+    private int mBitmapMod = 0;
+    private int mTick = 0;
+    //    private Modes mMode = Modes.BITMAP;
+    private Modes mMode = Modes.CROSSHAIRS;
+    //    private Modes mMode = Modes.DOTS;
+    private Ssd1306 mScreen;
+
+    private Handler mHandler = new Handler();
+    private Bitmap mBitmap;
+    private Runnable mDrawRunnable = new Runnable() {
+        /**
+         * Updates the display and tick counter.
+         */
+        @Override
+        public void run() {
+            // exit Runnable if the device is already closed
+            if (mScreen == null) {
+                return;
+            }
+            mTick++;
+            try {
+                switch (mMode) {
+                    case DOTS:
+                        drawExpandingDots();
+                        break;
+                    case BITMAP:
+                        drawMovingBitmap();
+                        break;
+                    case CROSSHAIRS:
+                    default:
+                        drawCrosshairs();
+                        break;
+                }
+                mScreen.show();
+                mHandler.postDelayed(this, 1000 / FPS);
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during screen update", e);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,18 +105,26 @@ public class MainActivity extends Activity {
             Log.i(TAG, "List of available ports: " + portList);
         }
 
-        try (Ssd1306 oledDisplay = new Ssd1306(I2C_DEVICE_NAME)) {
-            oledDisplay.begin();
-            Paint paint = new Paint();
-
-            paint.setUnderlineText(true);
-            paint.setFakeBoldText(true);
-            paint.setTextSize(8f);
-            oledDisplay.getCanvas().drawText("View Group 01", 0f, 0f, paint);
-            oledDisplay.displayImage();
+//        try (Ssd1306tmp oledDisplay = new Ssd1306tmp(I2C_DEVICE_NAME)) {
+//            oledDisplay.begin();
+//            Paint paint = new Paint();
+//
+//            paint.setUnderlineText(true);
+//            paint.setFakeBoldText(true);
+//            paint.setTextSize(8f);
+//            oledDisplay.getCanvas().drawText("View Group 01", 0f, 0f, paint);
+//            oledDisplay.displayImage();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        try {
+            mScreen = new Ssd1306(I2C_DEVICE_NAME);
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(TAG, "Error while opening screen", e);
+            throw new RuntimeException(e);
         }
+        Log.d(TAG, "OLED screen activity created");
+        mHandler.post(mDrawRunnable);
     }
 
     @Override
@@ -76,22 +132,90 @@ public class MainActivity extends Activity {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
 
-        if (spiDevice != null) {
-            try {
-                spiDevice.close();
-                spiDevice = null;
-            } catch (IOException e) {
-                Log.w(TAG, "Unable to close SPI device", e);
-            }
+        // remove pending runnable from the handler
+        mHandler.removeCallbacks(mDrawRunnable);
+        // Close the device.
+        try {
+            mScreen.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error closing SSD1306", e);
+        } finally {
+            mScreen = null;
         }
+    }
 
-        if (i2cDevice != null) {
-            try {
-                i2cDevice.close();
-                i2cDevice = null;
-            } catch (IOException e) {
-                Log.w(TAG, "Unable to close I2C device", e);
+    /**
+     * Draws crosshair pattern.
+     */
+    private void drawCrosshairs() {
+        mScreen.clearPixels();
+        int y = mTick % mScreen.getLcdHeight();
+        for (int x = 0; x < mScreen.getLcdWidth(); x++) {
+            mScreen.setPixel(x, y, true);
+            mScreen.setPixel(x, mScreen.getLcdHeight() - (y + 1), true);
+        }
+        int x = mTick % mScreen.getLcdWidth();
+        for (y = 0; y < mScreen.getLcdHeight(); y++) {
+            mScreen.setPixel(x, y, true);
+            mScreen.setPixel(mScreen.getLcdWidth() - (x + 1), y, true);
+        }
+    }
+
+    /**
+     * Draws expanding and contracting pixels.
+     */
+    private void drawExpandingDots() {
+        if (mExpandingPixels) {
+            for (int x = 0; x < mScreen.getLcdWidth(); x++) {
+                for (int y = 0; y < mScreen.getLcdHeight() && mMode == Modes.DOTS; y++) {
+                    mScreen.setPixel(x, y, (x % mDotMod) == 1 && (y % mDotMod) == 1);
+                }
+            }
+            mDotMod++;
+            if (mDotMod > mScreen.getLcdHeight()) {
+                mExpandingPixels = false;
+                mDotMod = mScreen.getLcdHeight();
+            }
+        } else {
+            for (int x = 0; x < mScreen.getLcdWidth(); x++) {
+                for (int y = 0; y < mScreen.getLcdHeight() && mMode == Modes.DOTS; y++) {
+                    mScreen.setPixel(x, y, (x % mDotMod) == 1 && (y % mDotMod) == 1);
+                }
+            }
+            mDotMod--;
+            if (mDotMod < 1) {
+                mExpandingPixels = true;
+                mDotMod = 1;
             }
         }
+    }
+
+    /**
+     * Draws a BMP in one of three positions.
+     */
+    private void drawMovingBitmap() {
+        if (mBitmap == null) {
+            mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.flower);
+        }
+        // Move the bmp every few ticks
+        if (mTick % BITMAP_FRAMES_PER_MOVE == 0) {
+            mScreen.clearPixels();
+            // Move the bitmap back and forth based on mBitmapMod:
+            // 0 - left aligned
+            // 1 - centered
+            // 2 - right aligned
+            // 3 - centered
+            int diff = mScreen.getLcdWidth() - mBitmap.getWidth();
+            int mult = mBitmapMod == 3 ? 1 : mBitmapMod; // 0, 1, or 2
+            int offset = mult * (diff / 2);
+            BitmapHelper.setBmpData(mScreen, offset, 0, mBitmap, false);
+            mBitmapMod = (mBitmapMod + 1) % 4;
+        }
+    }
+
+    enum Modes {
+        CROSSHAIRS,
+        DOTS,
+        BITMAP
     }
 }
--- app/src/main/java/it/oltrenoi/ampkernelui/oled/Ssd1306.java
+++ app/src/main/java/it/oltrenoi/ampkernelui/oled/Ssd1306.java
@@ -1,273 +1,282 @@
 package it.oltrenoi.ampkernelui.oled;
 
+/**
+ * Created by kruntz on 07/02/17.
+ */
+
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.util.Log;
 
 import com.google.android.things.pio.I2cDevice;
 import com.google.android.things.pio.PeripheralManagerService;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.Arrays;
 
+/**
+ * Driver for controlling the SSD1306 OLED display.
+ */
 public class Ssd1306 implements Closeable {
-
-    private static final String TAG = Ssd1306.class.getSimpleName();
-
-    // I2C Slave Address
-    private static final int I2C_ADDRESS = 0x78;
-
-    // Display size
-    private static final int DISPLAY_WIDTH = 128;
-    private static final int DISPLAY_HEIGHT = 64;
-
+    /**
+     * I2C address for this peripheral
+     */
+    public static final int I2C_ADDRESS = 0x3C;
+    public static final int I2C_ADDRESS_ALT = 0x3D;
+    private static final String TAG = "Ssd1306";
+    // Screen configuration constants.
+    private static final int LCDWIDTH = 128;
+    private static final int LCDHEIGHT = 64;
+    // Protocol constants
+    private static final int COMMAND_ACTIVATE_SCROLL = 0x2F;
+    private static final int COMMAND_DEACTIVATE_SCROLL = 0x2E;
+    private static final int COMMAND_RIGHT_HORIZONTAL_SCROLL = 0x26;
+    private static final int COMMAND_LEFT_HORIZONTAL_SCROLL = 0x27;
+    private static final int COMMAND_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
+    private static final int COMMAND_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
     private static final int COMMAND_DISPLAY_ON = 0xAF;
     private static final int COMMAND_DISPLAY_OFF = 0xAE;
-
-    private static final int SET_STARTLINE = 0x40;
-
-    private static final int INIT_SEGREMAP = 0xA0;
-
-    private static final int INIT_COMSCANDEC = 0xC8;
-    private static final int INIT_DUTY_CYCLE_1_64 = 0x3F;
-
+    private static final int COMMAND_START_LINE = 0x40;
+    private static final int DATA_OFFSET = 1;
+    private static final int INIT_CHARGE_PUMP = 0x8D;
     private static final int INIT_CLK_DIV = 0xD5;
-    private static final int INIT_RESISTER_RATIO = 0x80;
-
-    private static final int INIT_MEMORY_MODE = 0x20;
-    private static final int INIT_MEMORY_HORIZ = 0x00;
-
+    private static final int INIT_COMSCAN_DEC = 0xC8;
+    private static final int INIT_DISPLAY_NO_OFFSET = 0x0;
     private static final int INIT_DISPLAY_OFFSET = 0xD3;
-    private static final int INIT_DISPLAY_NO_OFFSET = 0x00;
+    private static final int INIT_DUTY_CYCLE_1_64 = 0x3F;
+    private static final int INIT_MEMORY_ADDRESSING_HORIZ = 0x0;
+    private static final int INIT_RESISTER_RATIO = 0x80;
+    private static final int INIT_SEGREMAP = 0xA1;
+    private static final int INIT_SET_MEMORY_ADDRESSING_MODE = 0x20;
+    private static final byte SSD1306_DISPLAY_WRITE = (byte) 0xA4;
+    private static final byte[] INIT_PAYLOAD = new byte[]{
+            // Step 1: Start with the display off
+            0, (byte) COMMAND_DISPLAY_OFF,
 
-    private static final int SSD1306_SETCONTRAST = 0x81;
+            // Step 2: Set up the required communication / power settings
+            0, (byte) INIT_SEGREMAP,
+            0, (byte) INIT_COMSCAN_DEC,
+            0, (byte) INIT_DUTY_CYCLE_1_64,
+            0, (byte) INIT_CLK_DIV,
+            0, (byte) INIT_RESISTER_RATIO,
 
-    private static final int SSD1306_DISPLAYALLON_RESUME = 0xA4;
-    private static final int SSD1306_DISPLAYALLON = 0xA5;
+            // Step 3: Set display input configuration and start. This will start the display all
+            // on, you must transmit START_LINE to present the memory-based mBuffer to the screen
+            0, (byte) INIT_SET_MEMORY_ADDRESSING_MODE,
+            0, (byte) INIT_MEMORY_ADDRESSING_HORIZ,
+            0, (byte) INIT_DISPLAY_OFFSET,
+            0, (byte) INIT_DISPLAY_NO_OFFSET,
+            0, (byte) COMMAND_DISPLAY_ON,
+            0, (byte) INIT_CHARGE_PUMP
+    };
+    // Holds the i2c payload.
+    private final byte[] mBuffer = new byte[((LCDWIDTH * LCDHEIGHT) / 8) + 1];
+    private I2cDevice mI2cDevice;
 
-    private static final int SSD1306_NORMALDISPLAY = 0xA6;
-    private static final int SSD1306_INVERTDISPLAY = 0xA7;
-
-    private static final int SSD1306_SETCOMPINS = 0xDA;
-    private static final int SSD1306_SETVCOMDETECT = 0xDB;
-    private static final int SSD1306_SETPRECHARGE = 0xD9;
-    private static final int COMMAND_SETMULTIPLEX = 0xA8;
-    private static final int SSD1306_SETLOWCOLUMN = 0x00;
-    private static final int SSD1306_SETHIGHCOLUMN = 0x10;
-
-    private static final int SSD1306_COLUMNADDR = 0x21;
-    private static final int SSD1306_PAGEADDR = 0x22;
-
-    private static final int SSD1306_COMSCANINC = 0xC0;
-    private static final int SSD1306_CHARGEPUMP = 0x8D;
-    private static final int SSD1306_EXTERNALVCC = 0x1;
-    private static final int SSD1306_SWITCHCAPVCC = 0x2;
-
-    // Scroll
-    private static final short COMMAND_ACTIVATE_SCROLL = 0x2F;
-    private static final short COMMAND_DEACTIVATE_SCROLL = 0x2E;
-    private static final short COMMAND_RIGHT_HORIZONTAL_SCROLL = 0x26;
-    private static final short COMMAND_LEFT_HORIZONTAL_SCROLL = 0x27;
-    private static final short COMMAND_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
-    private static final short COMMAND_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
-    private static final short SET_VERTICAL_SCROLL_AREA = 0xA3;
-
-    private Bitmap bitmap;
-    private Canvas canvas;
-    private int pages;
-    private I2cDevice i2cDevice;
-    private byte[] buffer;
-
-    public Ssd1306(String i2DeviceName) throws IOException {
-        this.i2cDevice = new PeripheralManagerService().openI2cDevice(i2DeviceName, I2C_ADDRESS);
-        init();
-        this.pages = (DISPLAY_HEIGHT / 8);
-        this.buffer = new byte[DISPLAY_WIDTH * this.pages];
-
-        this.bitmap = Bitmap.createBitmap(DISPLAY_WIDTH, DISPLAY_HEIGHT, Bitmap.Config.ALPHA_8);
-        this.canvas = new Canvas(this.bitmap);
+    /**
+     * Create a new Ssd1306 driver connected to the named I2C bus
+     *
+     * @param i2cName I2C bus name the display is connected to
+     * @throws IOException
+     */
+    public Ssd1306(String i2cName) throws IOException {
+        this(i2cName, I2C_ADDRESS);
     }
 
-    public Canvas getCanvas() {
-        return canvas;
+    /**
+     * Create a new Ssd1306 driver connected to the named I2C bus and address
+     *
+     * @param i2cName    I2C bus name the display is connected to
+     * @param i2cAddress I2C address of the display
+     * @throws IOException
+     */
+    public Ssd1306(String i2cName, int i2cAddress) throws IOException {
+        I2cDevice device = new PeripheralManagerService().openI2cDevice(i2cName, i2cAddress);
+        try {
+            init(device);
+        } catch (IOException | RuntimeException e) {
+            try {
+                close();
+            } catch (IOException | RuntimeException ignored) {
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * Create a new Ssd1306 driver connected to the given device
+     *
+     * @param device I2C device of the display
+     * @throws IOException
+     */
+    /*package*/ Ssd1306(I2cDevice device) throws IOException {
+        init(device);
+    }
+
+    /**
+     * Recommended start sequence for initializing the communications with the OLED display.
+     * WARNING: If you change this code, power cycle your display before testing.
+     *
+     * @throws IOException
+     */
+    private void init(I2cDevice device) throws IOException {
+        mI2cDevice = device;
+        BitmapHelper.bmpToBytes(mBuffer, DATA_OFFSET,
+                Bitmap.createBitmap(LCDWIDTH, LCDHEIGHT, Bitmap.Config.ARGB_8888),
+                false);
+        mBuffer[0] = (byte) COMMAND_START_LINE;
+
+        // Recommended initialization sequence based on http://goo.gl/VSu0C8
+        mI2cDevice.write(INIT_PAYLOAD, INIT_PAYLOAD.length);
+        stopScroll();
     }
 
     @Override
     public void close() throws IOException {
-        Log.d(TAG, "close");
-        if (this.i2cDevice != null) {
+        if (mI2cDevice != null) {
             try {
-                this.i2cDevice.close();
+                mI2cDevice.close();
             } finally {
-                this.i2cDevice = null;
+                mI2cDevice = null;
             }
         }
     }
 
-    private void init() {
-        Log.d(TAG, "init");
-
-        // Recommended initialization sequence based on http://goo.gl/VSu0C8
-        // Step 1: Start with the display off
-        this.command(COMMAND_DISPLAY_OFF);
-
-        // Step 2: Set up the required communication / power settings
-        this.command(INIT_SEGREMAP | 0x1);
-
-        this.command(INIT_COMSCANDEC);
-        this.command(INIT_DUTY_CYCLE_1_64);
-
-        this.command(INIT_CLK_DIV);
-        this.command(INIT_RESISTER_RATIO);
-
-        // Step 3: Set display input configuration and start.
-        // This will start the display all on
-        // START_LINE present the memory-based mBuffer to the screen
-        this.command(INIT_MEMORY_MODE);
-        this.command(INIT_MEMORY_HORIZ);
-
-        this.command(INIT_DISPLAY_OFFSET);
-        this.command(INIT_DISPLAY_NO_OFFSET);
-
-        this.command(COMMAND_DISPLAY_ON);
-
-        this.command(SSD1306_CHARGEPUMP);
-
-        // OPTIONALS
-        //        this.command(COMMAND_SETMULTIPLEX);
-        //        this.command(SSD1306_CHARGEPUMP);
-        //        this.command(0x10);
-        //        this.command(SSD1306_SETCOMPINS);
-        //        this.command(0x12);
-        //        this.command(SSD1306_SETCONTRAST);
-        //        this.command(0x9F);
-        //        this.command(SSD1306_SETPRECHARGE);
-        //        this.command(0x22);
-        //        this.command(SSD1306_SETVCOMDETECT);
-        //        this.command(0x40);
-        //        this.command(SSD1306_DISPLAYALLON_RESUME);
-        //        this.command(SSD1306_NORMALDISPLAY);
-        // OPTIONALS
-
-        this.command(SET_STARTLINE);
-    }
-
-    public void begin() {
-        this.init();
-        this.command(COMMAND_DISPLAY_ON);
-        this.clear();
-        this.display();
-    }
-
     /**
-     * Sends the buffer to the display
+     * @return the width of the display
      */
-    private synchronized void display() {
-        this.command(SSD1306_COLUMNADDR);
-        this.command(0);
-        this.command(DISPLAY_WIDTH - 1);
-        this.command(SSD1306_PAGEADDR);
-        this.command(0);
-        this.command(this.pages - 1);
-
-        this.data(this.buffer);
+    public int getLcdWidth() {
+        return LCDWIDTH;
     }
 
     /**
-     * Clears the buffer by creating a new byte array
+     * @return the height of the display
      */
-    private void clear() {
-        this.buffer = new byte[DISPLAY_WIDTH * this.pages];
-    }
-
-    public void setContrast(byte contrast) {
-        this.command(SSD1306_SETCONTRAST);
-        this.command(contrast);
-    }
-
-    public void invertDisplay(boolean invert) {
-        if (invert) {
-            this.command(SSD1306_INVERTDISPLAY);
-        } else {
-            this.command(SSD1306_NORMALDISPLAY);
-        }
-    }
-
-    public void scrollHorizontally(boolean left, int start, int end) {
-        this.command(left ? COMMAND_LEFT_HORIZONTAL_SCROLL : COMMAND_RIGHT_HORIZONTAL_SCROLL);
-        this.command(0);
-        this.command(start);
-        this.command(0);
-        this.command(end);
-        this.command(1);
-        this.command(0xFF);
-        this.command(COMMAND_ACTIVATE_SCROLL);
-    }
-
-    public void stopScroll() {
-        this.command(COMMAND_DEACTIVATE_SCROLL);
-    }
-
-    public int getWidth() {
-        return DISPLAY_WIDTH;
-    }
-
-    public int getHeight() {
-        return DISPLAY_HEIGHT;
+    public int getLcdHeight() {
+        return LCDHEIGHT;
     }
 
     /**
-     * Sets one pixel in the current buffer
+     * Clears all pixel data in the display buffer. This will be rendered the next time
+     * {@link #show()} is called.
+     */
+    public void clearPixels() {
+        Arrays.fill(mBuffer, DATA_OFFSET, mBuffer.length, (byte) 0);
+    }
+
+    /**
+     * Sets a specific pixel in the display buffer to on or off. This will be rendered the next time
+     * {@link #show()} is called.
      *
-     * @param x     X position
-     * @param y     Y position
-     * @param white White or black pixel
-     * @return True if the pixel was successfully set
+     * @param x  The horizontal coordinate.
+     * @param y  The vertical coordinate.
+     * @param on Set to true to enable the pixel; false to disable the pixel.
      */
-    private boolean setPixel(int x, int y, boolean white) {
-        if (x < 0 || x > DISPLAY_WIDTH || y < 0 || y > DISPLAY_HEIGHT) {
-            return false;
+    public void setPixel(int x, int y, boolean on) throws IllegalArgumentException {
+        if (x < 0 || y < 0 || x >= LCDWIDTH || y >= LCDHEIGHT) {
+            throw new IllegalArgumentException("pixel out of bound:" + x + "," + y);
         }
-
-        if (white) {
-            this.buffer[x + (y / 8) * DISPLAY_WIDTH] |= (1 << (y & 7));
+        if (on) {
+            mBuffer[DATA_OFFSET + x + ((y / 8) * LCDWIDTH)] |= (1 << y % 8);
         } else {
-            this.buffer[x + (y / 8) * DISPLAY_WIDTH] &= ~(1 << (y & 7));
-        }
-
-        return true;
-    }
-
-    public synchronized void displayImage() {
-        for (int y = 0; y < DISPLAY_HEIGHT; y++) {
-            for (int x = 0; x < DISPLAY_WIDTH; x++) {
-                this.setPixel(x, y, Color.alpha(this.bitmap.getPixel(x, y)) > 0);
-            }
-        }
-
-        this.display();
-    }
-
-    private void command(int command) {
-        this.i2cWrite(0, command);
-    }
-
-    private void data(byte[] data) {
-        try {
-            this.i2cDevice.write(data, data.length);
-        } catch (IOException e) {
-            Log.w(TAG, "Unable write buffer to I2C device", e);
+            mBuffer[DATA_OFFSET + x + ((y / 8) * LCDWIDTH)] &= ~(1 << y % 8);
         }
     }
 
-    private void i2cWrite(int register, int value) {
-        value &= 0xFF;
-        byte _8bitVal = (byte) value;
-        try {
-            this.i2cDevice.writeRegByte(register, _8bitVal);
-        } catch (IOException e) {
-            Log.w(TAG, "Unable to access I2C device", e);
+    /**
+     * Turns the display on and off.
+     *
+     * @param on Set to true to enable the display; set to false to disable the display.
+     * @throws IOException
+     * @throws IllegalStateException
+     */
+    public void setDisplayOn(boolean on) throws IOException, IllegalStateException {
+        if (mI2cDevice == null) {
+            throw new IllegalStateException("I2C Device not open");
         }
+        if (on) {
+            mI2cDevice.writeRegByte(0, (byte) COMMAND_DISPLAY_ON);
+        } else {
+            mI2cDevice.writeRegByte(0, (byte) COMMAND_DISPLAY_OFF);
+        }
+    }
+
+    /**
+     * Renders the current pixel data to the screen.
+     *
+     * @throws IOException
+     * @throws IllegalStateException
+     */
+    public void show() throws IOException, IllegalStateException {
+        if (mI2cDevice == null) {
+            throw new IllegalStateException("I2C Device not open");
+        }
+        mI2cDevice.writeRegByte(0, SSD1306_DISPLAY_WRITE);
+        mI2cDevice.write(mBuffer, mBuffer.length);
+    }
+
+    /**
+     * Start scrolling the display horizontally.
+     *
+     * @param startY     The starting row to scroll.
+     * @param finishY    The ending row to scroll.
+     * @param scrollMode Configures the direction that the display contents scroll.
+     * @throws IOException
+     * @throws IllegalStateException
+     */
+    public void startScroll(int startY, int finishY, ScrollMode scrollMode)
+            throws IOException, IllegalStateException {
+        if (mI2cDevice == null) {
+            throw new IllegalStateException("I2C Device not open");
+        }
+
+        int scrollModeVal = 0;
+        switch (scrollMode) {
+            case RightHorizontal:
+                scrollModeVal = COMMAND_RIGHT_HORIZONTAL_SCROLL;
+                break;
+            case LeftHorizontal:
+                scrollModeVal = COMMAND_LEFT_HORIZONTAL_SCROLL;
+                break;
+            case VerticalLeftHorizontal:
+                scrollModeVal = COMMAND_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL;
+                break;
+            case VerticalRightHorizontal:
+                scrollModeVal = COMMAND_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL;
+                break;
+            default:
+                // Should never happen
+                break;
+        }
+        byte[] payload = new byte[]{
+                0, (byte) scrollModeVal,
+                0, 0,
+                0, (byte) startY,
+                0, 0,
+                0, (byte) finishY,
+                0, 0,
+                0, (byte) 0xFF,
+                0, COMMAND_ACTIVATE_SCROLL
+        };
+        mI2cDevice.write(payload, payload.length);
+    }
+
+    /**
+     * Stop scrolling the display
+     *
+     * @throws IOException
+     * @throws IllegalStateException
+     */
+    public void stopScroll() throws IOException, IllegalStateException {
+        if (mI2cDevice == null) {
+            throw new IllegalStateException("I2C Device not open");
+        }
+        mI2cDevice.writeRegByte(0, (byte) COMMAND_DEACTIVATE_SCROLL);
+    }
+
+    public enum ScrollMode {
+        RightHorizontal,
+        LeftHorizontal,
+        VerticalRightHorizontal,
+        VerticalLeftHorizontal
     }
 }
